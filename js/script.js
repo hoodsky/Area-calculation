6673666e90a8021c4056b9abe0b07a07767c6cab
hoodsky <Hoodsky.ks@gmail.com>
2022-06-30 22:05:56 +0300

    0.01

--- js/script.js
+++ js/script.js
@@ -6,20 +6,24 @@ let tileNum = document.getElementById('tile-num'); //количество пли
 let input = document.querySelectorAll('input'); //
 let numInBox = document.getElementById('num-in-box') //штук в коробке
 let numBox = document.getElementById('num-box'); //количество коробок
+let boxesPlus = document.getElementById('boxes-plus');
 
 //input.onchange = () => {
 //  calcArea()
 //}
 tileArea.onchange = () => {
     calcArea()
-    numBox.value = tileNum.value / numInBox.value
+    numBox.value = Math.floor(tileNum.value / numInBox.value);
+    boxesPlus.value = Math.ceil(tileNum.value % numInBox.value);
 }
 tileNum.onchange = () => {
     let calcOneTileArea = (tileWidth.value / 100) * (tileHeight.value / 100);
     tileArea.value = calcOneTileArea * tileNum.value;
     // tileNum.value = numInBox.value * numBox.value;
+    console.log(Math.ceil(tileNum.value % numInBox.value))
+    numBox.value = Math.floor(tileNum.value / numInBox.value)
+    boxesPlus.value = Math.ceil(tileNum.value % numInBox.value);
 
-    numBox.value = tileNum.value / numInBox.value
 }
 tileWidth.onchange = () => {
     calcArea()
@@ -32,14 +36,18 @@ tileHeight.onchange = () => {
 const calcArea = () => {
     let calcOneTileArea = (tileWidth.value / 100) * (tileHeight.value / 100);
     oneTileArea.innerText = `Площадь одной плитки ${calcOneTileArea} м²`;
-    tileNum.value = tileArea.value / calcOneTileArea
+    tileNum.value = Math.ceil(tileArea.value / calcOneTileArea);
     // console.log('change')
 }
 numInBox.onchange = () => {
-    numBox.value = tileNum.value / numInBox.value
+
+    numBox.value = Math.floor(tileNum.value / numInBox.value);
     tileNum.value = numInBox.value * numBox.value;
     let calcOneTileArea = (tileWidth.value / 100) * (tileHeight.value / 100);
     tileArea.value = calcOneTileArea * tileNum.value;
+    console.log(Math.ceil(tileNum.value % numInBox.value));
+    boxesPlus.value = Math.ceil(tileNum.value % numInBox.value);
+
 }
 
 numBox.onchange = () => {
