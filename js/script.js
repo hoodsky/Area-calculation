55b337dd146fd56f71003746ac53a02e2c8b8564
hoodsky <Hoodsky.ks@gmail.com>
2022-06-30 16:17:02 +0300

    0.00002

--- js/script.js
+++ js/script.js
@@ -2,24 +2,32 @@ let tileWidth = document.getElementById('tile-width');
 let tileHeight = document.getElementById('tile-height');
 let oneTileArea = document.getElementById('one-tile-area');
 let tileArea = document.getElementById('tile-area');
-let tileNumerosity = document.getElementById('tile-numerosity');
+let tileNum = document.getElementById('tile-num');
 let input = document.querySelectorAll('input');
+let boxNum = document.getElementById('numerosity')
+
 
 //input.onchange = () => {
 //  calcArea()
 //}
-
- tileWidth.onchange = () => {
-  calcArea()
- }
+tileArea.onchange = () => {
+    calcArea()
+}
+tileNum.onchange = () => {
+    let calcOneTileArea = (tileWidth.value / 100) * (tileHeight.value / 100);
+    tileArea.value = calcOneTileArea * tileNum.value;
+}
+tileWidth.onchange = () => {
+    calcArea()
+}
 
 tileHeight.onchange = () => {
-calcArea()
+    calcArea()
 }
 
 const calcArea = () => {
- oneTileArea.innerText = `Площадь одной плитки ${(tileWidth.value/100) * (tileHeight.value/100)} м²`;
-
-
-// console.log('change')
+    let calcOneTileArea = (tileWidth.value / 100) * (tileHeight.value / 100);
+    oneTileArea.innerText = `Площадь одной плитки ${calcOneTileArea} м²`;
+    tileNum.value = tileArea.value / calcOneTileArea
+    // console.log('change')
 }
