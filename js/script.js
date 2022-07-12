a79828a5305f09e309e32742ae100ea19bc91113
hoodsky <Hoodsky.ks@gmail.com>
2022-07-12 21:47:27 +0300

    0.1 beta

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
 "use strict";
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function () {
+        navigator.serviceWorker.register('/sw.js').then(
+            function (registration) {
+                // Registration was successful
+                console.log('ServiceWorker registration successful with scope: ', registration.scope);
+            },
+            function (err) {
+                // registration failed :(
+                console.log('ServiceWorker registration failed: ', err);
+            });
+    });
+}
 
 let tileWidth = document.getElementById('tile-width'); //ширина плитки
 let tileHeight = document.getElementById('tile-height'); //высота плитки
